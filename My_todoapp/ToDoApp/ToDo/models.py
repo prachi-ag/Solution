90a5fbe789d40f4f0123a4d6843649ba05f21bf9
prachi <prachiagarwal30111998@gmail.com>
2019-03-14 15:52:23 +0530

    Added MyToDo App in Django

---
+++ My_todoapp/ToDoApp/ToDo/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Todo(models.Model):
+    text=models.CharField(max_length=400)
+    complete=models.BooleanField(default=False)
+    def _str_(self):
+        return self.text
---
+++ My_todoapp/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/prachi30111998/PycharmProjects/My_todoapp/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
